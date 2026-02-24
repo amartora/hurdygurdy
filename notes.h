b31b10fc20c167cfc1fd3310c477a33f4f26cf2e
amartora <alexmartorano@gmail.com>
2026-02-24 17:18:08 -0500

    Added note frequency header file

---
+++ notes.h
@@ -0,0 +1,129 @@
+#define C0 16.35
+#define Dd0 17.32
+#define D0 18.35 
+#define Eb0 19.45
+#define E0 20.60
+#define F0 21.83
+#define Gb0 23.12
+#define G0 24.50
+#define Ab0 25.96
+#define A0 27.50
+#define Bb0 29.14
+#define B0 30.87
+
+#define C1 32.70
+#define Dd1 34.65
+#define D1 36.71
+#define Eb1 38.89
+#define E1 41.20
+#define F1 43.65
+#define Gb1 46.25
+#define G1 49.00
+#define Ab1 51.91
+#define A1 55.00
+#define Bb1 58.27
+#define B1 61.74
+
+#define C2 65.41
+#define Dd2 69.30  
+#define D2 73.42
+#define Eb2 77.78
+#define E2 82.41
+#define F2 87.31 
+#define Gb2 92.50
+#define G2 98.00
+#define Ab2 103.83
+#define A2 110.00
+#define Bb2 116.54
+#define B2 123.47
+
+#define C3 130.81
+#define Dd3 138.59
+#define D3 146.83
+#define Eb3 155.56
+#define E3 164.81
+#define F3 174.61
+#define Gb3 185.00  
+#define G3 196.00
+#define Ab3 207.65
+#define A3 220.00
+#define Bb3 233.08
+#define B3 246.94
+
+#define C4 261.63
+#define Dd4 277.18
+#define D4 293.66
+#define Eb4 311.13
+#define E4 329.63
+#define F4 349.23
+#define Gb4 369.99
+#define G4 392.00
+#define Ab4 415.30
+#define A4 440.00
+#define Bb4 466.16
+#define B4 493.88
+
+
+#define C5 523.25
+#define Dd5 554.37
+#define D5 587.33
+#define Eb5 622.25
+#define E5 659.25
+#define F5 698.46 
+#define Gb5 739.99
+#define G5 783.99
+#define Ab5 830.61
+#define A5 880.00
+#define Bb5 932.33
+#define B5 987.77
+
+#define C6 1046.50
+#define Dd6 1108.73  
+#define D6 1174.66
+#define Eb6 1244.51
+#define E6 1318.51
+#define F6 1396.91
+#define Gb6 1479.98
+#define G6 1567.98
+#define Ab6 1661.22
+#define A6 1760.00
+#define Bb6 1864.66  
+#define B6 1975.53
+
+#define C7 2093.00
+#define Dd7 2217.46
+#define D7 2349.32
+#define Eb7 2489.02
+#define E7 2637.02
+#define F7 2793.83
+#define Gb7 2959.96
+#define G7 3135.96
+#define Ab7 3322.44
+#define A7 3520.00
+#define Bb7 3729.31
+#define B7 3951.07
+
+#define C8 4186.01
+#define Dd8 4434.92
+#define D8 4698.63
+#define Eb8 4978.03
+#define E8 5274.04
+#define F8 5587.65
+#define Gb8 5919.91  
+#define G8 6271.93
+#define Ab8 6644.88
+#define A8 7040.00
+#define Bb8 7458.62
+#define B8 7902.13
+
+static float allNotes[108] {
+  C0, Dd0, D0, Eb0, E0, F0, Gb0, G0, Ab0, Bb0, B0,
+  C1, Dd1, D1, Eb1, E1, F1, Gb1, G1, Ab1, Bb1, B1,
+  C2, Dd2, D2, Eb2, E2, F2, Gb2, G2, Ab2, Bb2, B2,
+  C3, Dd3, D3, Eb3, E3, F3, Gb3, G3, Ab3, Bb3, B3,
+  C4, Dd4, D4, Eb4, E4, F4, Gb4, G4, Ab4, Bb4, B4,
+  C5, Dd5, D5, Eb5, E5, F5, Gb5, G5, Ab5, Bb5, B5,
+  C6, Dd6, D6, Eb6, E6, F6, Gb6, G6, Ab6, Bb6, B6,
+  C7, Dd7, D7, Eb7, E7, F7, Gb7, G7, Ab7, Bb7, B7,
+  C8, Dd8, D8, Eb8, E8, F8, Gb8, G8, Ab8, Bb8, B8,
+};
